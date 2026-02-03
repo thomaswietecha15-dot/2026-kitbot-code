1ce5fc96a024801a530510ab76fb9d5f96aa4499
thomaswietecha15-dot <thomaswietecha15@gmail.com>
2026-02-03 17:41:13 -0500

    New kit bot code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
  * the TimedRobot documentation. If you change the name of this class or the package after creating
@@ -17,6 +18,13 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
+
+  
+    @Override
+    public void robotInit() {
+        // Initialization happens in RobotContainer
+        new RobotContainer();
+    }
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -42,6 +50,8 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+       // Runs the scheduler for commands
+        CommandScheduler.getInstance().run();
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
@@ -75,6 +85,7 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
+      CommandScheduler.getInstance().cancelAll();
   }
 
   /** This function is called periodically during operator control. */
